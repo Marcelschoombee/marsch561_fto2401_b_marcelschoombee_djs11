d640ffa7c8bdc9e429ce6adc474bc9f395358060
Marcel Schoombee <marcelsch01@yahoo.com>
2024-06-18 10:03:01 +0200

    working on Routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import Card from '../components/Cards.jsx';
+import { fetchPreview } from '../api.js'
+
+export default function Home() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const previewData = await fetchPreview(); // Fetch data from API
+                setData(previewData); // Update state with fetched data
+                setLoading(false); // Update loading state
+            } catch (error) {
+                setError(error.message); // Handle fetch error
+                setLoading(false); // Update loading state
+            }
+        };
+
+        fetchData(); // Call fetchData function on component mount
+    }, []);
+
+    if (loading) return <div>Loading...</div>; // Render loading indicator if data is still loading
+    if (error) return <div>Error: {error}</div>
+    return (
+        <section className="cards-list">
+            {data.map(item => (
+                <Card
+                    key={item.id}
+                    title={item.title}
+                    description={item.description}
+                    genres={item.genres} // Assuming genres is an array of genre IDs
+                    image={item.image} // Image URL from API data
+                    url={item.url} // Assuming url is the URL for the card link
+                />
+            ))}
+        </section>
+    )
+}
